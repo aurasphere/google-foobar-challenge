ed3978fbe7aac8e69d58aa9d772e353a1681a632
Donato Rimenti <donatohan.rimenti@gmail.com>
2020-06-20 16:55:25 +0200

    Added 2.1 solution.

---
+++ Level 2/2.1 Don't Get Volunteered!/Solution.java
@@ -0,0 +1,53 @@
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class Solution {
+
+    public static int solution(int src, int dest) {
+        int movesCount = 0;
+        Set<Integer> visitedSquares = Collections.singleton(src);
+        while (!visitedSquares.contains(dest)) {
+            visitedSquares = visitedSquares.parallelStream()
+                .map(Solution::getNextMovesFromSquare)
+                .flatMap(Set::parallelStream)
+                .collect(Collectors.toSet());
+            movesCount++;
+        }
+        return movesCount;
+    }
+
+    private static Set<Integer> getNextMovesFromSquare(int square) {
+        int modSquare = square % 8;
+        Set<Integer> nextMoves = new HashSet<Integer>();
+        if (modSquare < 7) {
+            if (modSquare < 6) {
+                nextMoves.add(square + 10);
+                nextMoves.add(square - 6);
+            }
+            nextMoves.add(square + 17);
+            nextMoves.add(square - 15);
+        }
+        if (modSquare > 0) {
+            if (modSquare > 1) {
+                nextMoves.add(square - 10);
+                nextMoves.add(square + 6);
+            }
+            nextMoves.add(square - 17);
+            nextMoves.add(square + 15);
+        }
+        // Purge moves outside the checkboard.
+        nextMoves.removeIf(n -> n > 63 || n < 0);
+        return nextMoves;
+    }
+
+    public static void main(String... args) {
+        int srcOne = 19, destOne = 36, outputOne = 1;
+        int srcTwo = 0, destTwo = 1, outputTwo = 3;
+
+        assert solution(srcOne, destOne) == outputOne;
+        assert solution(srcTwo, destTwo) == outputTwo;
+    }
+
+}
